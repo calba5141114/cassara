5d10cc21a4b511b840e3c5d2e9187f1acf00ec09
admin <admin@admins-MacBook-Pro.local>
2018-05-19 15:55:45 -0700

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.set("view engine", "pug");
+
+app.set("views", __dirname + "/public/views");
+
+// sending off the index.page
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
+
